6763912c098df2ff5b22764c45e0d1affc09e520
Vishnu Gamini <gamini.vishnu@gmail.com>
2024-01-26 11:08:09 +0530

    fixed rendering issue

--- my_project/app.py
+++ my_project/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='pages')
 
 @app.route('/')
 def home():
-    return 'Welcome to the Home Page'
+    return render_template("home.html")
 
 @app.route('/scanning')
 def scanning():
